af038bd8d7e55b9b85a95546c722bee00413829c
Sathish151617 <134375863+Sathish151617@users.noreply.github.com>
2023-06-01 16:52:28 +0530

    Add files via upload

---
+++ src/main/java/com/mscomm/departmentservice/entity/Department.java
@@ -0,0 +1,25 @@
+package com.mscomm.departmentservice.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "departments")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Department {
+
+	  @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+	    private String Name;
+	    private String description;
+	    private String typeofpet;
+	    private String quality;
+	    private String price;
+	    
+}
